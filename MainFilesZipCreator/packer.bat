2497452eaeb2d889768c1c1507fd404e5f3e86cd
Arkhorse <20694721+Arkhorse@users.noreply.github.com>
2020-12-19 12:05:32 -0400

    add multi choice to choose Stable or Nightly, ensured DEL actually works

--- MainFilesZipCreator/packer.bat
+++ MainFilesZipCreator/packer.bat
@@ -10,16 +10,37 @@ rem Change the gameVersion, deployDate, XVMversion variables before each run.
 @set "gameVersion=1.11.0.0"
 @set "deployDate=2020-12-19"
 @set "zipFileName=LogicalDependency_XVM_Main"
-@set "XVMbranch=Nightly"
 @set "XVMversion=v8.7.1_0005"
 
-@md XVM
+Echo "Nightly or Stable?"
+    :start
+        ECHO.
+            echo "0. Stable"
+            echo "1. Nightly"
+			set choice=
+			set /p choice=Which game are you testing for?  
+			if not '%choice%'=='' set choice=%choice:~0,1%
+			if '%choice%'=='0' goto Stable
+			if '%choice%'=='1' goto Nightly
+			ECHO "%choice%" is not valid, try again
+        echo.
+        goto start
+:Stable
+@set "XVMbranch=Stable"
+goto :Continue
 
-%unPack% *.zip -oXVM
-xcopy /v /s /y "_patch\" "XVM\_patch\"
-xcopy /v /y "xvm.xc" "XVM\res_mods\configs\xvm\"
+:Nightly
+@set "XVMbranch=Nightly"
 
-@cd XVM
+:Continue
+
+@md XVM_%XVMbranch%
+
+%unPack% *.zip -oXVM_%XVMbranch%
+xcopy /v /s /y "_patch\" "XVM_%XVMbranch%\_patch\"
+xcopy /v /y "xvm.xc" "XVM_%XVMbranch%\res_mods\configs\xvm\"
+
+@cd XVM_%XVMbranch%
 @DEL *.txt
 
 pause
@@ -50,6 +71,6 @@ xcopy /v /y XVM\%zipFileName%_%XVMbranch%_%XVMversion%_%gameVersion%_%deployDate
 
 rem clean up
 
-DEL XVM
+DEL /q /s XVM_%XVMbranch%
 
 pause
